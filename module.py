79258fa9228855db204880b3907eac2d9cd5f1a6
Jie Yu <universe_bright@jies-mac-mini.mynetworksettings.com>
2024-02-25 10:54:10 -0500

    add module

---
+++ module.py
@@ -0,0 +1,45 @@
+from nn import Layer, Linear, Relu
+from loss import MSELoss
+import logging
+logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s",
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+class Module:
+    def __init__(self) -> None:
+        self.head = Layer()
+        self.tail = Layer()
+        self.head.next_layer(self.tail)
+        self.tail.prev_layer(self.head)
+        self.n_layers = 0
+    
+    def add(self, layer):
+        # add layer before tail
+        curr = self.tail.prev_layer
+        curr.next_layer = layer
+        layer.prev_layer = curr
+        layer.next_layer = self.tail
+        self.tail.prev_layer = layer
+        self.n_layers += 1
+    
+    def forward(self, inputs):
+        if self.n_layers == 0:
+            logger.error("No layer is added to the module.")
+            return
+        curr = self.head.next_layer
+        values = inputs
+        while curr != self.tail:
+            values = curr(values)
+        return values
+
+    def backward(self, errors, learning_rate):
+        if self.n_layers == 0:
+            logger.error("No layer is added to the module.")
+            return
+        curr = self.tail.prev_layer
+        values = errors
+        while curr != self.head:
+            values = curr.backward(values)
+        return values
+        
--- nn.py
+++ nn.py
@@ -1,25 +1,64 @@
 import numpy as np
-class Linear:
-	def __init__(self, d_input, d_output):
-		self.d_input = d_input
-		self.d_output = d_output
-		self._weights = 2 * (np.random.rand(d_input, d_output) - 0.5)
-		self._bias = 2 * (np.random.rand(1, d_output) - 0.5)
+class Layer:
+    def __init__(self, prev=None, next=None):
+        self._prev = prev
+        self._next = next
+    
+    @property
+    def prev_layer(self):
+        return self._prev
+    
+    @prev_layer.setter
+    def prev_layer(self, layer):
+        self._prev = layer
+    
+    @property
+    def next_layer(self):
+        return self._next
+    
+    @next_layer.setter
+    def next_layer(self, layer):
+        self._next = layer
+        
+class Linear(Layer):
+    def __init__(self, d_input, d_output):
+        super().__init__()
+        self.d_input = d_input
+        self.d_output = d_output
+        self._weights = 2 * (np.random.rand(d_input, d_output) - 0.5)
+        self._bias = 2 * (np.random.rand(1, d_output) - 0.5)
+        self.inputs = None
+        self.outputs = None
 
-	def forward(self, X):
-		return X * self._weights + self._bias
-	
-	@property
-	def weights(self):
-		return self._weights
-	
-	@property
-	def bias(self):
-		return self._bias
+    def forward(self, X):
+        self.inputs = X
+        self.outputs = X * self._weights + self._bias
+        return self.outputs
+    
+    def backward(self, y_errors, learning_rate):
+        # Y = W * X + B
+        # given dE / dY, what's the dE/dW, dE/dB and dE/dX
+        # dE/dB = dE/dY
+        # dE/dW = dE/dY * dY/dW, where dY/dW = X
+        # dE/dX = dE/dY * dY/dX, where dY/dX = W
+        x_errors = np.dot(y_errors, self.weights.T)
+        weight_errors = np.dot(self.input.T, y_errors)
+
+        self.weights -= learning_rate * weight_errors
+        self.bias -= learning_rate * y_errors
+        return x_errors
+
+    @property
+    def weights(self):
+        return self._weights
+    
+    @property
+    def bias(self):
+        return self._bias
 
 class Relu:
-	def __init__(self):
-		pass
+    def __init__(self):
+        pass
 
-	def forward(self, X):
-		return np.maximum(0., X)
+    def forward(self, X):
+        return np.maximum(0., X)
